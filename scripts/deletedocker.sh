a804614aa6a768ab04e5112c2872879e3dc98398
Vinu Joseph <vinujosef@gmail.com>
2020-05-24 00:36:57 +0300

    script: delete all docker containers, images, volumes

---
+++ scripts/deletedocker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script to delete all docker containers, images and volumes
+
+echo "===> DELETE CONTAINERS"
+docker rm $(docker ps -a -q) -f
+
+echo "===> DELETE IMAGES"
+docker rmi $(docker images -q) -f
+
+echo "===> DELETE VOLUMES"
+docker volume rm $(docker volume ls) -f
